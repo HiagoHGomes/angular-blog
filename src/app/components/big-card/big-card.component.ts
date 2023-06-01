1892a16a76689af05e106e3194704efce905f633
Hiago Gomes <hiago_h3@hotmail.com>
2023-06-01 12:34:10 -0300

    Desacoplando os componentes da parte visual

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  @Input()
+  bigCardImg:string = "";
+  @Input()
+  bigCardTitle:string = "Entrevista: Avenged Sevenfold e a montanha-russa do novo álbum ‘Life Is But a Dream…’";
+  @Input()
+  bigCardDescription:string = "Johnny Christ revelou os bastidores do novo disco, retorno aos palcos e planos sobre o Brasil";
 
   constructor() { }
 
