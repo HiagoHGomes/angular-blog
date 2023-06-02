af306ff699911f694f3060ccdc7da115355b4780
Hiago Gomes <hiago_h3@hotmail.com>
2023-06-01 23:22:12 -0300

    Projeto concluído

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -7,12 +7,14 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
   @Input()
+  id:string = "0";
+  @Input()
   bigCardImg:string = "";
   @Input()
-  bigCardTitle:string = "Entrevista: Avenged Sevenfold e a montanha-russa do novo álbum ‘Life Is But a Dream…’";
+  bigCardTitle:string = "";
   @Input()
-  bigCardDescription:string = "Johnny Christ revelou os bastidores do novo disco, retorno aos palcos e planos sobre o Brasil";
-
+  bigCardDescription:string = "";
+ 
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+     "title": "Entrevista: Avenged Sevenfold e a montanha-russa do novo álbum ‘Life Is But a Dream…’",
+     "descripion": "Johnny Christ revelou os bastidores do novo disco, retorno aos palcos e planos sobre o Brasil",
+     "photoCover": "https://i0.wp.com/www.wikimetal.com.br/wp-content/uploads/2023/05/Avenged-Sevenfold-em-2023.-Credito-DivulgacaoKerrangBrian-Catelle-Arte-Wikimetal.png?resize=1170%2C658&ssl=1"
+   },
+   {
+    "id":"2",
+     "title": "O clássico do Black Sabbath cantado por Dio que inspirou música do Iron Maiden",
+     "descripion": "Lançado em março de 1982, The Number Of The Beast é um dos discos mais celebrados da história do heavy metal. O trabalho marca a estreia de Bruce Dickinson como vocalista do grupo e apresenta músicas marcantes, como a faixa-título, Hallowed Be Thy Name, Run To The Hills e Children Of The Damned.",
+     "photoCover": "https://whiplash.net/imagens_promo_22/dio_dreamers_never_die.jpg"
+   },
+   {
+    "id":"3",
+     "title": "Fãs reagem ao novo videoclipe da Crypta e elogiam produção do novo álbum",
+     "descripion": "A banda Crypta lançou recentemente o videoclipe de Lord of Ruins, música que estará presente no novo álbum Shades of Sorrow, que sai no dia 4 de agosto pela Napalm Records.",
+     "photoCover": "https://whiplash.net/imagens_promo_22/fernandalira_crypta_por_gustavo_maiato2.jpg"
+   },
+   {
+    "id":"4",
+     "title": "Os 15 maiores shows de rock da história segundo a Loudwire (Inclui um no Brasil)",
+     "descripion": "Os shows ao vivo desempenham um papel essencial na indústria da música, proporcionando experiências inigualáveis e conectando artistas e fãs de uma maneira única. Exemplos notáveis de shows que marcaram época incluem a lendária performance do Queen no Live Aid em 1985, que se tornou um marco na história do rock, e o icônico show do Pearl Jam no Lollapalooza de 1992, que solidificou a reputação da banda como uma das mais enérgicas e cativantes do cenário alternativo da época.",
+     "photoCover": "https://whiplash.net/imagens_promo_22/pearljam_promo.jpg"
+   },
+
+]
